c0fbeeaa19291b471fdd962444d12200bdb249fd
Tom McCauley <tpmccauley@gmail.com>
2013-07-18 14:58:21 +0100

    crossfilter now works (but hacky implementation), rm old files

--- elab/elab.js
+++ elab/elab.js
@@ -15,6 +15,7 @@ Elab.Dataset = Backbone.Model.extend({
 	idAttribute: "id"
 });
 
+/*
 Elab.Chart = Backbone.Model.extend({
     defaults: {
         name: "",
@@ -27,16 +28,13 @@ Elab.Chart = Backbone.Model.extend({
         brush: d3.svg.brush()
     },
 
-    getDomain: function() {
-        return this.get('domain');
-    },
-
     idAttribute: "id"
 });
 
 Elab.Charts = Backbone.Collection.extend({
     model: Elab.Chart
 });
+*/
 
 Elab.Datasets = Backbone.Collection.extend({
 	model: Elab.Dataset,
@@ -445,11 +443,14 @@ Elab.datasets.add(Elab.dielectron_dataset);
 Elab.datasets.add(Elab.hgammagamma_dataset);
 
 Elab.plots = new Elab.Plots();
+
+/*
 Elab.charts = new Elab.Charts();
 
 Elab.charts.on("add", function(ch) {
     console.log("Added chart: " + ch.get("name") + ", size: " + this.size());
 });
+*/
 
 Elab.DatasetPageView = Backbone.View.extend({
 	el: "#datasets",
@@ -566,12 +567,10 @@ Elab.ParameterTableView = Backbone.View.extend({
 Elab.CrossfilterView = Backbone.View.extend({
     template: _.template($('#crossfilter-template').html()),
 
-    brush: 
-
     render: function() {
-        console.log("There are " + this.collection.size() + " charts");
-        $("#crossfilter-plots").append(this.template({charts: this.collection.toJSON()}));
+        $("#crossfilter-plots").append(this.template({parameters: this.collection.toJSON()}));
 
+        /*
         this.collection.each(function(ch) {
             var name = ch.get('name');
 
@@ -579,10 +578,15 @@ Elab.CrossfilterView = Backbone.View.extend({
             var xmax = ch.get('dimension').top(1)[0][name];
             var xmin = ch.get('dimension').bottom(1)[0][name];
 
-            console.log(xmin + ', ' + xmax);
+            ch.set('x', d3.scale.linear().domain([xmin, xmax]).rangeRound([0, 400]));
+            ch.set('width',  ch.get('x').range()[1]);
+            ch.set('height', ch.get('y').range()[0]);
+            ch.get('y').domain([0, ch.get('group').top(1)[0].value]);
 
-            ch.set('x', d3.scale.linear().domain([xmin, xmax]).rangeRound([0, 400]))
+            console.log(ch.toJSON());
+
         }, this);
+        */
     }
 });
 
@@ -719,9 +723,207 @@ Elab.getData = function(data_url) {
         var data = eval(request.responseText);
         Elab.raw_data = data;
         Elab.crossfilter = crossfilter(data);
+        Elab.all = Elab.crossfilter.groupAll();
         console.log(Elab.crossfilter.size());
     });
 };
+
+// For now, before we properly factorize all of this,
+// follow closely from the crossfilter example.
+Elab.barChart = function() {
+    if (! Elab.barChart.id ) Elab.barChart.id = 0;
+
+    var margin = {top: 10, right: 10, bottom: 20, left: 10},
+        x,
+        y = d3.scale.linear().range([200, 0]),
+        id = Elab.barChart.id++,
+        axis = d3.svg.axis().orient("bottom"),
+        brush = d3.svg.brush(),
+        brushDirty,
+        dimension,
+        group,
+        round;
+
+    function chart(div) {
+        var width = x.range()[1],
+            height = y.range()[0];
+
+            y.domain([0, group.top(1)[0].value]);
+
+            div.each(function() {
+
+                var div = d3.select(this),
+                    g = div.select("g");
+
+                if ( g.empty() ) {
+                
+                    div.select(".title").append("a")
+                    .attr("href", "javascript:reset(" + id + ")")
+                    .attr("class", "reset")
+                    .text("reset")
+                    .style("display", "none");
+
+                    g = div.append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                    g.append("clipPath")
+                    .attr("id", "clip-" + id)
+                    .append("rect")
+                    .attr("width", width)
+                    .attr("height", height);
+
+                    g.selectAll(".bar")
+                    .data(["background", "foreground"])
+                    .enter().append("path")
+                    .attr("class", function(d) { return d + " bar"; })
+                    .datum(group.all());
+
+                    g.selectAll(".foreground.bar")
+                    .attr("clip-path", "url(#clip-" + id + ")");
+
+                    g.append("g")
+                    .attr("class", "axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(axis);
+
+                    var gBrush = g.append("g").attr("class", "brush").call(brush);
+                    gBrush.selectAll("rect").attr("height", height);
+                    gBrush.selectAll(".resize").append("path").attr("d", resizePath);
+                }
+
+                if ( brushDirty ) {
+                    brushDirty = false;
+                    g.selectAll(".brush").call(brush);
+                    div.select(".title a").style("display", brush.empty() ? "none" : null);
+            
+                    if ( brush.empty() ) {
+                        g.selectAll("#clip-" + id + " rect")
+                        .attr("x", 0)
+                        .attr("width", width);
+                    } else {
+                        var extent = brush.extent();
+                        g.selectAll("#clip-" + id + " rect")
+                        .attr("x", x(extent[0]))
+                        .attr("width", x(extent[1]) - x(extent[0]));
+                    }
+                }
+
+                g.selectAll(".bar").attr("d", barPath);
+            }); // end of div.each
+
+        function barPath(groups) {
+            var path = [],
+                i = -1,
+                n = groups.length,
+                d;
+
+            while (++i < n) {
+                d = groups[i];
+                path.push("M", x(d.key), ",", height, "V", y(d.value), "h9V", height);
+            }
+            return path.join("");
+        }
+
+        function resizePath(d) {
+            var e = +(d == "e"),
+                x = e ? 1 : -1,
+                y = height / 3;
+            return "M" + (.5 * x) + "," + y
+            + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6)
+            + "V" + (2 * y - 6)
+            + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y)
+            + "Z"
+            + "M" + (2.5 * x) + "," + (y + 8)
+            + "V" + (2 * y - 8)
+            + "M" + (4.5 * x) + "," + (y + 8)
+            + "V" + (2 * y - 8);
+        }
+    }
+
+    brush.on("brushstart.chart", function() {
+      var div = d3.select(this.parentNode.parentNode.parentNode);
+      div.select(".title a").style("display", null);
+    });
+
+    brush.on("brush.chart", function() {
+      var g = d3.select(this.parentNode),
+          extent = brush.extent();
+      if (round) g.select(".brush")
+          .call(brush.extent(extent = extent.map(round)))
+        .selectAll(".resize")
+          .style("display", null);
+      g.select("#clip-" + id + " rect")
+          .attr("x", x(extent[0]))
+          .attr("width", x(extent[1]) - x(extent[0]));
+      dimension.filterRange(extent);
+    });
+
+    brush.on("brushend.chart", function() {
+      if (brush.empty()) {
+        var div = d3.select(this.parentNode.parentNode.parentNode);
+        div.select(".title a").style("display", "none");
+        div.select("#clip-" + id + " rect").attr("x", null).attr("width", "100%");
+        dimension.filterAll();
+      }
+    });
+
+    chart.margin = function(_) {
+      if (!arguments.length) return margin;
+      margin = _;
+      return chart;
+    };
+
+    chart.x = function(_) {
+      if (!arguments.length) return x;
+      x = _;
+      axis.scale(x);
+      brush.x(x);
+      return chart;
+    };
+
+    chart.y = function(_) {
+      if (!arguments.length) return y;
+      y = _;
+      return chart;
+    };
+
+    chart.dimension = function(_) {
+      if (!arguments.length) return dimension;
+      dimension = _;
+      return chart;
+    };
+
+    chart.filter = function(_) {
+      if (_) {
+        brush.extent(_);
+        dimension.filterRange(_);
+      } else {
+        brush.clear();
+        dimension.filterAll();
+      }
+      brushDirty = true;
+      return chart;
+    };
+
+    chart.group = function(_) {
+      if (!arguments.length) return group;
+      group = _;
+      return chart;
+    };
+
+    chart.round = function(_) {
+      if (!arguments.length) return round;
+      round = _;
+      return chart;
+    };
+
+    return d3.rebind(chart, brush, "on");
+};
+
+Elab.charts = [];
 
 Elab.Router = Backbone.Router.extend({
 	routes: {
@@ -764,7 +966,9 @@ Elab.Router = Backbone.Router.extend({
         Elab.getData(Elab.datasets.get(Elab.selected_dataset).get('url'));
 
         Elab.plots.reset();
-        Elab.charts.reset();
+        //Elab.charts.reset();
+
+        Elab.charts.length = 0;
 
         Elab.datasets
             .get(Elab.selected_dataset)
@@ -780,14 +984,19 @@ Elab.Router = Backbone.Router.extend({
 
                 Elab.plots.add(plot);
 
-                var dimension = Elab.crossfilter.dimension(function(d) {return d[name];});
+                var dimension = Elab.crossfilter.dimension(function(d) {return +d[name];});
                 //console.log(dimension.top(1));
 
                 var group = dimension.group(function(d) {return Math.floor(d/0.1)*0.1;});
                 //console.log(group);
 
-                var chart = new Elab.Chart({name:name, dimension:dimension, group:group});
-                Elab.charts.add(chart);
+                var xmax = dimension.top(1)[0][name];
+                var xmin = dimension.bottom(1)[0][name];
+
+                Elab.charts.push(Elab.barChart().dimension(dimension).group(group).x(d3.scale.linear().domain([xmin, xmax]).rangeRound([0,500])));
+
+                //var chart = new Elab.Chart({name:name, dimension:dimension, group:group});
+                //Elab.charts.add(chart);
             });
 
         Elab.emptyAll();
@@ -796,8 +1005,39 @@ Elab.Router = Backbone.Router.extend({
         Elab.flot_plots_view.collection = Elab.plots;
         Elab.flot_plots_view.render();
 
-        Elab.crossfilter_view.collection = Elab.charts;
+        Elab.crossfilter_view.collection = Elab.datasets.get(Elab.selected_dataset).get('parameters').getSelected();
         Elab.crossfilter_view.render();
+        //Elab.crossfilter_view.collection = Elab.charts;
+        //Elab.crossfilter_view.render();
+
+        var formatNumber = d3.format(",d");
+
+        var chart = d3.selectAll(".chart")
+                .data(Elab.charts)
+                .each(function(chart) { chart.on("brush", renderAll).on("brushend", renderAll); });
+
+        function render(method) {
+            d3.select(this).call(method);
+        }
+
+        function renderAll() {
+            chart.each(render);
+            d3.select("#active").text(formatNumber(Elab.all.value()));
+        }
+
+        /*
+        window.filter = function(filters) {
+            filters.forEach(function(d, i) { Elab.charts[i].filter(d); });
+            renderAll();
+        };
+
+        window.reset = function(i) {
+            Elab.charts[i].filter(null);
+            renderAll();
+        };
+        */
+
+        renderAll();
 	}
 });
 
